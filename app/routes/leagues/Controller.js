9353cfab4133a41a8f750c290f0f8d2f9a13b046
Sasha Tatar <sasa.tatar@gmail.com>
2017-09-25 23:53:33 +0200

    ...

--- app/routes/leagues/Controller.js
+++ app/routes/leagues/Controller.js
@@ -3,12 +3,12 @@ import { History, Controller } from 'cx/ui';
 export default class extends Controller {
     onInit() {
         this.store.set('leagues', [
-            { id: 1, name: 'A', players: 20 },
-            { id: 2, name: 'B', players: 21 }
+            { id: 'A', name: 'A', players: 20 },
+            { id: 'B', name: 'B', players: 21 }
         ])
     }
 
     onClickLeague(e, {store}) {
-        History.pushState({}, '', `~/league/${store.get('$record.id')}`);
+        History.pushState({}, '', `~/leagues/${store.get('$record.id')}`);
     }
 }
